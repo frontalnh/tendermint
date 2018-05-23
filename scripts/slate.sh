b4d3f903c826c38c353b8ceaf28dfee9a2c6dd40
Zach Ramsay <zach.ramsay@gmail.com>
2018-05-23 11:09:36 -0400

    try it this way

--- scripts/slate.sh
+++ scripts/slate.sh
@@ -6,9 +6,6 @@ if [ "$CIRCLE_BRANCH" == "" ]; then
 	echo 1
 fi
 
-# need develop to compare against
-git fetch origin develop
-
 # check for changes in the `rpc/core` directory
 did_rpc_change=$(git diff --name-status $CIRCLE_BRANCH develop | grep rpc/core)
 
